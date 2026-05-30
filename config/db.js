b3fc5c2f6f0c25fa78fc03ca0e4d681a263c258c
Irfana Thasni <irfanathasni652022@gmail.com>
2026-05-30 15:31:25 +0530

    configured database connection

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
+require("dotenv").config()
 const mongoose = require("mongoose")
 const connectDB = async () => {
             try{
-                await mongoose.connect("mongodb://localhost:27017/week7")
+                await mongoose.connect("MONGODB_URI")
                 console.log("Database connected")
             }
             catch(error){
                 console.log("Database connected faild")
             }
 }
-
 module.exports = connectDB
